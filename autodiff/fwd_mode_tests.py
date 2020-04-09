e4bfaec33f64c6557decdaffcb4a8e54b48972e6
Abhihshek Sharma <shaabhishek@users.noreply.github.com>
2020-04-09 19:45:54 -0400

    converted files to module

--- autodiff/fwd_mode_tests.py
+++ autodiff/fwd_mode_tests.py
@@ -1,6 +1,6 @@
 import torch
 
-from fwd_mode import FwdNumber, exp, sin, TensorNumber
+from autodiff.fwd_mode import FwdNumber, exp, sin, TensorNumber
 
 
 def f1(x1, x2, scalar_constructor):
@@ -19,10 +19,14 @@ def test_with_pytorch(f):
     x1, x2 = torch.Tensor([1.]).requires_grad_(), torch.Tensor([1.])
     out = f(x1, x2, TensorNumber())
     out.backward()
+    # PyTorch implements reverse mode diff
+    # This means that the gradient of `out` wrt `x1` sits in x1's Tensor
     print(out.data, x1.grad)
 
 
 def test_with_fwdnumber(f):
     x1, x2 = FwdNumber(1, 1), FwdNumber(1)
     out = f(x1, x2, FwdNumber)
+    # FwdNumber implements forward mode diff
+    # This means that the gradient of `out` wrt `x1` sits in `out`'s Tensor
     print(out)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fwd_mode_tests import f1, f2, test_with_pytorch, test_with_fwdnumber
+from autodiff.fwd_mode_tests import f1, f2, test_with_pytorch, test_with_fwdnumber
 
 if __name__ == '__main__':
     for f in [f1, f2]:
